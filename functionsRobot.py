5d437d0bfe1aca326cf147ffcdcbff1a49056ac1
Alexandr Pirogov <88452033+eduAlexandrPirogov@users.noreply.github.com>
2022-08-06 12:44:47 +0300

    Update functionsRobot.py

--- functionsRobot.py
+++ functionsRobot.py
@@ -4,12 +4,15 @@ x = 0.0
 y = 0.0
 angle = 0
 
-def __init__():
-        x = 0.0
-        y = 0.0
-        angle = 0
+isClearing = False
+states = ["water", "soap", "brush"]
+currentState = states[0]
 
-def turn( angle):
+
+robotClearing = clearing()
+robotMovement = movement()
+
+def turn(angle):
         if angle + angle <= 360:
             angle += angle
         elif angle + angle > 360:
@@ -17,15 +20,10 @@ def turn( angle):
         elif angle - angle < 360:
             angle = 0 - (angle + angle-360)
     
-def move( sm):
+def move(sm):
         x += sm * math.cos(angle)
         y += sm * math.sin(angle)
-
-
-def __init__():
-        isClearing = False
-        states = ["water", "soap", "brush"]
-        currentState = states[0]
+        print("Robot arrived at ", x, " ", y)
 
 def start():
         isClearing = True
@@ -33,33 +31,7 @@ def start():
 def stop():
         isClearing = False
 
-def set( state):
+def set(state):
         currentState = state
 
-
-def __init__():
-        robotClearing = clearing()
-        robotMovement = movement()
-
-def move( sm):
-        robotMovement.move(sm)
-        print("Robot arrived at ", robotMovement.x, " ", robotMovement.y)
-
-def turn( angle):
-        robotMovement.turn(angle)
-        print("Robot turned at ", angle)
-
-    #setting clearing state
-    #better clear methods make as enums/classes
-def set( state):
-        robotClearing.set(state)
-        print("Robot has set state " + state)
-
-def start():
-        robotClearing.start()
-        print("Robot has started washing with " + robotClearing.currentState)
-
-def stop():
-        robotClearing.stop()
-        print("Robot has stoped washing with " + robotClearing.currentState)
 
