f7a85968579daf12e99b522736b25fa57a03745f
Alexandr Pirogov <88452033+eduAlexandrPirogov@users.noreply.github.com>
2022-08-23 17:45:10 +0300

    Add files via upload

--- functionalDependecyInjection/client.py
+++ functionalDependecyInjection/client.py
@@ -2,7 +2,7 @@ import server_api
 
 state = server_api.create_robot()
 
-server_api.do_robot(('move 50', 'turn 90'), state)
+server_api.do_robot(server_api.move, 50, state)
 
-#deliever = server_api.create_deliever_robot()
-#server_api.do_robot(server_api.move_deliever, 50, deliever)
+deliever = server_api.create_deliever_robot()
+server_api.do_robot(server_api.move_deliever, 50, deliever)
